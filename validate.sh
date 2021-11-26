e4c679582d5b7d3508a074601460a3e4670d6241
Mathieu Lacage <mathieu.lacage@gmail.com>
2021-11-26 15:37:23 +0100

    validation script

---
+++ validate.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+npm install @apidevtools/swagger-cli
+for f in user subbot webchat
+do
+  echo "Validating $f";
+  ./node_modules/@apidevtools/swagger-cli/bin/swagger-cli.js validate $f.yaml;
+done
