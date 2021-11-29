076e660bf3a7b3db94082d656794bcdb03bdc119
Mathieu Lacage <mathieu.lacage@gmail.com>
2021-11-29 16:02:49 +0100

    export API

--- validate.sh
+++ validate.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 npm install @apidevtools/swagger-cli
-for f in user subbot webchat
+for f in user subbot webchat exports
 do
   echo "Validating $f";
   ./node_modules/@apidevtools/swagger-cli/bin/swagger-cli.js validate $f.yaml;
