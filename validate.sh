e88353bf6a01bea80901ba7198b32d51de830eae
Mathieu Lacage <mathieu.lacage@gmail.com>
2022-01-11 10:44:41 +0100

    update yaml filename

--- validate.sh
+++ validate.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 npm install --prefix ./ @apidevtools/swagger-cli
-for f in user subbot webchat exports synchronize
+for f in user subbot inmessage exports synchronize
 do
   echo "Validating $f";
   ./node_modules/@apidevtools/swagger-cli/bin/swagger-cli.js validate $f.yaml;
