658dc37164f53ee2fcecb3c0e44d74cb391b603b
Mathieu Lacage <mathieu.lacage@gmail.com>
2022-03-04 07:28:10 +0100

    First custom field import

--- validate.sh
+++ validate.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 npm install --prefix ./ @apidevtools/swagger-cli
-for f in user subbot inmessage exports synchronize
+for f in user subbot inmessage exports synchronize custom-field
 do
   echo "Validating $f";
   ./node_modules/@apidevtools/swagger-cli/bin/swagger-cli.js validate $f.yaml;
