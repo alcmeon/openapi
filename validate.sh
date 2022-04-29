67bf529329fc51c8cff3710426addeb8460c5647
Mathieu Lacage <mathieu.lacage@gmail.com>
2022-04-29 09:57:26 +0200

    start scenarios

--- validate.sh
+++ validate.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 npm install --prefix ./ @apidevtools/swagger-cli
-for f in user subbot inmessage exports synchronize custom-field
+for f in user subbot inmessage exports synchronize custom-field scenarios
 do
   echo "Validating $f";
   ./node_modules/@apidevtools/swagger-cli/bin/swagger-cli.js validate $f.yaml;
