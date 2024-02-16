574dede20af2731ee7ad7c7e08b298459c3f69ce
Alexis Kartmann <alexis.kartmann@alcmeon.com>
2024-02-16 17:33:21 +0100

    Add AI Bot

--- validate.sh
+++ validate.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 #npm install --prefix ./ @apidevtools/swagger-cli
-for f in user subbot subbot-api inmessage exports synchronize custom-field scenarios application embed ai ai-api
+for f in user subbot subbot-api inmessage exports synchronize custom-field scenarios application embed ai ai-api ai-bot
 do
   echo "Validating $f";
   ./node_modules/@apidevtools/swagger-cli/bin/swagger-cli.js validate $f.yaml;
