2b3d4dc93daa3926ba8d7a83b21b131f4206d892
Alexis Kartmann <alexis.kartmann@alcmeon.com>
2024-02-16 18:04:00 +0100

    Merge AI Webhooks

--- validate.sh
+++ validate.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 #npm install --prefix ./ @apidevtools/swagger-cli
-for f in user subbot subbot-api inmessage exports synchronize custom-field scenarios application embed ai ai-api ai-bot
+for f in user subbot subbot-api inmessage exports synchronize custom-field scenarios application embed ai ai-api
 do
   echo "Validating $f";
   ./node_modules/@apidevtools/swagger-cli/bin/swagger-cli.js validate $f.yaml;
