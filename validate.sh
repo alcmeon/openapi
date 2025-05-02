42516b605c2432350ce933c5179f81d242e6ad4d
Romain <80467679+r-o-mainalcmeon@users.noreply.github.com>
2025-05-02 16:51:48 +0200

    SP-8913 Document OAuth Client Credentials Grant Flow (#18)

--- validate.sh
+++ validate.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 #npm install --prefix ./ @apidevtools/swagger-cli
-for f in user subbot subbot-api inmessage exports synchronize custom-field scenarios application embed ai ai-api kakaotalk-send-api core-api
+for f in user subbot subbot-api inmessage exports synchronize custom-field scenarios application embed ai ai-api kakaotalk-send-api core-api auth
 do
   echo "Validating $f";
   ./node_modules/@apidevtools/swagger-cli/bin/swagger-cli.js validate $f.yaml;
