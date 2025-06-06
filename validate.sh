da3cc4f1e7d9c69adfb8693b72307693b924b95e
Romain <romain.bouteloup@alcmeon.com>
2025-06-06 17:15:40 +0200

    SP-9207 Add bearer token auth to documented API endpoints, centralize auth definitions and allow anonymous OAuth token requests.

--- validate.sh
+++ validate.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 #npm install --prefix ./ @apidevtools/swagger-cli
-for f in user subbot subbot-api inmessage exports synchronize custom-field scenarios application embed ai ai-api kakaotalk-send-api core-api auth
+for f in user subbot subbot-api inmessage exports synchronize custom-field scenarios application embed ai ai-api kakaotalk-send-api core-api oauth
 do
-  echo "Validating $f";
+  echo "- Validating $f";
   ./node_modules/@apidevtools/swagger-cli/bin/swagger-cli.js validate $f.yaml;
 done
