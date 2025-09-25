8d3c17ec8b9caa270af32b770c7123e4944470bf
Adrien Jacquot <adrien.jacquot@alcmeon.com>
2025-09-25 17:04:02 +0200

    Add Send Answer API routes

--- validate.sh
+++ validate.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 #npm install --prefix ./ @apidevtools/swagger-cli
-for f in user subbot subbot-api inmessage exports synchronize custom-field scenarios application embed ai ai-api kakaotalk-initiate-conversation-api core-api oauth whatsapp-initiate-conversation-api
+for f in user subbot subbot-api inmessage exports synchronize custom-field scenarios application embed ai ai-api kakaotalk-initiate-conversation-api core-api oauth whatsapp-initiate-conversation-api answer
 do
   echo "- Validating $f";
   ./node_modules/@apidevtools/swagger-cli/bin/swagger-cli.js validate $f.yaml;
